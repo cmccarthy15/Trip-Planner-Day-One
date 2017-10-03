272d1f1bcd052046bfe598ad205bb540754ee51c
Caryn McCarthy <cmccarthy15@gmail.com>
2017-10-03 11:16:59 -0400

    defer is not my friend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('We will not test it now though... ');
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY21jY2FydGh5MTUiLCJhIjoiY2o4YnFiN3RyMDBuNjJ3c2Y4dzhkMnIzNyJ9.ebxoBjxotfIr5-5EIsZ1hA';
+
+const map = new mapboxgl.Map({
+  container: "map1",
+  center: [-74.009, 40.705], // FullStack coordinates
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10' // other styles: light-v9, dark-v9, satellite-streets-v10, satellite-v9
+});
